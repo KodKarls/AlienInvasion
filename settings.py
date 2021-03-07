285aa084441146f483dd2b33655791adce0252b6
Carolinii <carolinii.contact@gmail.com>
2021-03-07 12:49:48 +0100

    add shooting system to the ship

--- settings.py
+++ settings.py
@@ -10,3 +10,10 @@ class Settings():
 
         # Ustawienia dotyczące statku.
         self.ship_speed = 1.0
+
+        # Ustawienia dotyczące pocisku.
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
